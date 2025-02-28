1062d813ff9338246af3870592d38da14a1efcf8
AMRITESH240304 <amriteshk778@gmail.com>
2025-02-28 13:39:05 +0530

    host

--- backend/main.py
+++ backend/main.py
@@ -11,4 +11,4 @@ def read_root():
     return {"message": "Welcome to the FastAPI application!"}
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="10.3.251.71", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
